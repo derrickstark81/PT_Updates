56d8c830cff8050fe84a4e4640c984ed80182fb7
Derrick Stark <derrick.a.stark.1@gmail.com>
2025-09-04 12:01:58 -0500

    Upload __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,19 @@
+'''*********************************************************************************************************************************
+Tool Name: metadata.py
+Version: Python 3.11.10
+Author: DAS (OWRB GIS)
+Description: 
+            Module to make modules importable.
 
+History: 
+            Initial coding - DAS 20250617
+            
+Usage: Use in main script
+Comments:
+************************************************************************************************************************************'''
+
+from .database import SDEDatabase
+from .metadata import MetadataManager
+from .utilities import timeit, validate_path
+
+__all__ = ["SDEDatabase", "MetadataManager", "timeit", "validate_path"]
